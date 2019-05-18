b74c5825b89c5fff6659d73ec7e57bc5c1e8703a
davesabra <davesabra@yahoo.com>
2019-05-18 01:36:24 +0100

    Add files via upload
    'Bob' message and reply exercise
    final iteration

---
+++ rust/lib.rs
@@ -0,0 +1,20 @@
+pub fn reply(message: &str) -> &str {
+    // clear front and end whitespaces
+    let message = message.trim();
+    // patternise message
+    let is_question: bool = message.ends_with("?");
+    let is_shouted: bool =
+        message.chars().any(char::is_uppercase) 
+           && !message.chars().any(char::is_lowercase); 
+    
+    // match message pattern to Bob's replies
+    match message.is_empty() {
+        true  => "Fine. Be that way!",
+        false => match (is_question, is_shouted) {
+            (true, true)   => "Calm down, I know what I'm doing!",
+            (false, true)  => "Whoa, chill out!",
+            (true, false)  => "Sure.",
+            (false, false) => "Whatever.",
+                  }
+    }
+}
---
+++ rust/main.rs
@@ -0,0 +1,12 @@
+use bob::reply;
+
+fn main() {
+    // failed tests
+    println!("{:?}", reply("WHAT THE HELL WERE YOU THINKING?"));
+
+    //println!("{:?}", reply("1,2,3"));
+
+    //println!("{:?}", reply(":)?"));
+
+    //println!("{:?}", reply("4?"));
+}
