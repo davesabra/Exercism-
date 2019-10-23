b332786196456e5a5a13a0a2531918801896328a
davesabra <davesabra@yahoo.com>
2019-10-23 03:11:10 +0100

    Add files via upload
    Space Age exercism
    a solution
    uses macro-rules!

--- rust/lib.rs
+++ rust/lib.rs
@@ -1,33 +1,54 @@
-use std::collections::HashSet;
-use itertools::Itertools;
-use unicode_segmentation::UnicodeSegmentation;
 
+const SECONDS_IN_EARTH_YEAR: f64 = 31557600.0;
 
-       
- pub fn anagrams_for<'a>(word: &str, possibles: &[&'a str]) -> HashSet<&'a str> {  
+pub struct Duration {
+    seconds: f64,
+}
+
+impl From<u64> for Duration {
 	
-	 let sort = |stringy: &str| {            
-	    let stringy = stringy                        
-                      .graphemes(true)
-                      .collect::<String>()
-                      .to_lowercase();  
-                     
-         stringy.chars()
-                   .sorted()
-                   .collect::<String>()
-	 };                                                                   
-    
-     let lower_word = word.to_lowercase();  // pre-computing these avoids performance penalty 
-     let sorted_word = &sort(word);             // of re-calculating them every iteration
-    
-    possibles                                          
-        .iter()
-        .copied()      
-        .filter(|candidate| lower_word != candidate.to_lowercase()  )    
-        .filter(|candidate| *sorted_word == sort(candidate))   
-        .collect()	
- }
-              
+    fn from(s: u64) -> Self { 
+        Duration { seconds: s as f64 / SECONDS_IN_EARTH_YEAR }    //normalise to Earth time 
+    }
+}
 
- 
-    
+pub trait Planet {
+    const ORBITAL: f64; // assoc const of trait Planet
+   
+    fn years_during(d: &Duration) -> f64 {
+        (d.seconds  / Self::ORBITAL)
+    }
+}
+
+macro_rules! planet {
+    ($name:ident, $orbital:expr) => {       // when I find this pattern  ( see lines 33...  )
+        
+        pub struct $name;                         // I insert this code
+       
+        impl Planet for $name { 
+			const ORBITAL: f64 = $orbital; }           //   and I insert this code implementing Planet for Duration struct
+    }
+}     
+
+planet!(Mercury, 0.2408467);     // this call will expand into the code block of the macro
+planet!(Venus, 0.61519726);         // this call will expand into the code block of the macro
+planet!(Earth, 1.0);                      // .....as above
+planet!(Mars, 1.8808158);
+planet!(Jupiter, 11.862615);
+planet!(Saturn, 29.447498);
+planet!(Uranus, 84.016846);
+planet!(Neptune, 164.79132);
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	 
